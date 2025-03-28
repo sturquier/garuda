0e7f23b5596d04e5078012b0355588f54280ca5c
sturquier <turquiersimon@hotmail.fr>
2025-03-28 23:28:20 +0100

    Refactor tabBar

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,7 +1,15 @@
 import { Ionicons } from '@expo/vector-icons';
 import { Tabs } from 'expo-router';
 import React from 'react';
-import { ColorValue } from 'react-native';
+import { useTranslation } from 'react-i18next';
+import {
+  ColorValue,
+  Pressable,
+  StyleProp,
+  StyleSheet,
+  Text,
+  TextStyle,
+} from 'react-native';
 
 import { Header } from '@/components';
 import { COLORS } from '@/theme/colors';
@@ -9,52 +17,56 @@ import { COLORS } from '@/theme/colors';
 export default function TabLayout() {
   const ICON_SIZE = 28;
 
+  const { t } = useTranslation();
+
+  const iconName = (routeName: string): keyof typeof Ionicons.glyphMap => {
+    if (routeName === 'search') return 'search-outline';
+    if (routeName === 'profile') return 'person-circle-outline';
+
+    return 'home-outline';
+  };
+
   const iconColor = (focused: boolean): ColorValue =>
     focused ? COLORS.brown : COLORS.black;
 
+  const labelColor = (focused: boolean): ColorValue =>
+    focused ? COLORS.brown : COLORS.black;
+
+  const labelStyle = (focused: boolean): StyleProp<TextStyle> => ({
+    color: labelColor(focused),
+  });
+
   return (
     <Tabs
-      screenOptions={{
+      screenOptions={({ route }) => ({
+        tabBarStyle: styles.tabBar,
         header: (props) => <Header {...props} />,
-        tabBarShowLabel: false,
-      }}
+        tabBarButton: (props) => <Pressable {...props} />,
+        tabBarIcon: ({ focused }) => (
+          <Ionicons
+            name={iconName(route.name)}
+            color={iconColor(focused)}
+            size={ICON_SIZE}
+          />
+        ),
+        tabBarLabel: ({ focused }) => (
+          <Text style={labelStyle(focused)}>{t(`routes.${route.name}`)}</Text>
+        ),
+      })}
     >
-      <Tabs.Screen
-        name='index'
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name='home-outline'
-              color={iconColor(focused)}
-              size={ICON_SIZE}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name='search'
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name='search-outline'
-              color={iconColor(focused)}
-              size={ICON_SIZE}
-            />
-          ),
-        }}
-      />
-      <Tabs.Screen
-        name='profile'
-        options={{
-          tabBarIcon: ({ focused }) => (
-            <Ionicons
-              name='person-circle-outline'
-              color={iconColor(focused)}
-              size={ICON_SIZE}
-            />
-          ),
-        }}
-      />
+      <Tabs.Screen name='index' />
+      <Tabs.Screen name='search' />
+      <Tabs.Screen name='profile' />
     </Tabs>
   );
 }
+
+const styles = StyleSheet.create({
+  tabBar: {
+    position: 'absolute',
+    height: 60,
+    marginHorizontal: 12,
+    marginBottom: 16,
+    borderRadius: 20,
+  },
+});
